9c0a99e43ae5ee28674eaaf176158c8bbe54dc0c
Joshua Gomez <joshuago78@yahoo.com>
2014-01-04 16:59:49 -0800

    implemented List minimum function for Day2 of Prolog exercises

--- Prolog/day2.pl
+++ Prolog/day2.pl
@@ -5,9 +5,35 @@ rev([First, Second], [Second, First]).
 rev([Head|Tail], Final) :- rev(Tail, Twist), append(Twist, [Head], Final).
 
 /* ex:
-rev([1,2,3,4], What).
+| ?- rev([1,2,3,4], What).
 
 What = [4,3,2,1] ?
+
+yes
+
+*/
+
+
+/* Do: Find the smallest element of a list. */
+
+min([Head|[]], Head).
+min([First, Second], Min) :- 
+    First < Second -> Min = First ;
+    Min = Second.
+min([First|[Second|Rest]], Min) :-
+    min([First, Second], Temp),
+    min([Temp|Rest], Min).
+
+/* ex:
+
+| ?- min([9,8,7,6,5,4,3,2,1,0,-1], W).
+
+W = -1 ? 
+
+yes
+| ?- min([1], Min).
+
+Min = 1 ? 
 
 yes
 
